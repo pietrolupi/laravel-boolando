da4131d3a6200dd9424844856041acfa1927d907
pietrolupi <pietro.lupi92@gmail.com>
2023-11-17 15:43:44 +0100

    [init]: predisposizione struttura base

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,24 @@
+<header>
+
+    <div class="container">
+      <div class="menu">
+        <ul>
+          <li ><a href="#">Home</a></li>
+          <li ><a href="#">Home</a></li>
+          <li ><a href="#">Home</a></li>
+          <li ><a href="#">Home</a></li>
+        </ul>
+      </div>
+
+      <div class="logo">
+        <img src="/public/img/boolean-logo.png" alt="logo">
+      </div>
+
+      <div class="icons">
+          <a href="#"><i class="fa-regular fa-user"></i></a>
+          <a href="#"><i class="fa-regular fa-heart"></i></a>
+          <a href="#"><i class="fa-solid fa-bag-shopping"></i></a>
+      </div>
+
+    </div>
+  </header>
