b7bfd93f442c30efb077c2462b9810164c82f663
pietrolupi <pietro.lupi92@gmail.com>
2023-11-17 16:44:27 +0100

    [add]: array di array products da cui stampare

---
+++ config/products.php
@@ -0,0 +1,88 @@
+<?php
+
+return [
+
+    [
+      "id" => 134,
+      "img"=> '../assets/img/1.webp',
+      "imgHover" => '../assets/img/1b.webp',
+      "discountPerc" => '-50%',
+      "ecological"=> true,
+      "brand"=> 'Levi \'s',
+      "name"=> 'RELAXED FIT TEE UNISEX',
+      "slug"=> 'relaxed-fit-tee-unisex',
+      "price"=> '14,99',
+      "exPrice"=> '29,99',
+      "discount"=> true
+    ],
+
+    [
+      "id" => 1345,
+      "img"=> '../assets/img/2.webp',
+      "imgHover"=> '../assets/img/2b.webp',
+      "discountPerc"=> '-30%',
+      "ecological"=> false,
+      "brand"=> 'Guess',
+      "name"=> 'ROSES TEE',
+      "slug"=> 'roses-tee',
+      "price"=> '20,99',
+      "exPrice"=> '29,99',
+      "discount"=> true
+    ],
+
+    [
+      "id" => 13456,
+      "img"=> '../assets/img/3.webp',
+      "imgHover"=> '../assets/img/3b.webp',
+      "discountPerc"=> '-30%',
+      "ecological"=> false,
+      "brand"=> 'Come Zucchero Filato',
+      "name"=> 'VOGLIA DI COLORI PASTELLO',
+      "slug"=> 'voglia-di-colori-pastello',
+      "price"=> '129,99',
+      "exPrice"=> '184,99',
+      "discount"=> true
+    ],
+
+    [
+      "id" => 17756,
+      "img"=> '../assets/img/4.webp',
+      "imgHover"=> '../assets/img/4b.webp',
+      "discountPerc"=> '-50%',
+      "ecological"=> true,
+      "brand"=> 'Levi \'s',
+      "name"=> 'TEE UNISEX',
+      "slug"=> 'tee-unisex',
+      "price"=> '14,99',
+      "exPrice"=> '29,99',
+      "discount"=> true
+    ],
+
+    [
+      "id" => 8956,
+      "img"=> '../assets/img/5.webp',
+      "imgHover"=> '../assets/img/5b.webp',
+      "discountPerc"=> 'none',
+      "ecological"=> false,
+      "brand"=> 'Maya Deluxe',
+      "name"=> 'STRIPE BODICE',
+      "slug"=> 'stripe-bodice',
+      "price"=> '14,99',
+      "exPrice"=> 'none',
+      "discount"=> false
+    ],
+
+    [
+      "id" => 82026,
+      "img"=> '../assets/img/6.webp',
+      "imgHover"=> '../assets/img/6b.webp',
+      "discountPerc"=> 'none',
+      "ecological"=> true,
+      "brand"=> 'Esprit',
+      "name"=> 'MAGLIONE - BLACK',
+      "slug"=> 'maglione-black',
+      "price"=> '29,99',
+      "exPrice"=> 'none',
+      "discount"=> false
+    ]
+    ];
